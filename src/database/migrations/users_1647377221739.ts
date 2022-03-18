8edbf945ff1a6b3690d8904cadac8860875658f7
Nathael Bonnal <kirua48000@gmail.com>
2022-03-18 14:29:27 +0100

    :white_check_mark: first version to DataStructure finished

---
+++ src/database/migrations/users_1647377221739.ts
@@ -0,0 +1,17 @@
+import { Migration, BaseMigration, Schema, Table } from '@discord-factory/storage-next'
+
+@Migration()
+export default class User_1647377221740 extends BaseMigration {
+  public tableName = 'users'
+
+  public async up (schema: Schema): Promise<any> {
+    return schema.createTable(this.tableName, (table: Table) => {
+      table.string('id').primary()
+      table.string('user_id').unique()
+    })
+  }
+
+  public async down (schema: Schema): Promise<any> {
+    return schema.dropTableIfExists(this.tableName)
+  }
+}
